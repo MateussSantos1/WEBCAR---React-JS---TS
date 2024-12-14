84339891d31680f6f2eefa8d459673617e54bab9
Matt <mateuservico@gmail.com>
2024-12-13 21:23:10 -0300

    barra de pesquisa da home

--- Webcar/src/components/header/index.tsx
+++ Webcar/src/components/header/index.tsx
@@ -7,7 +7,7 @@ export function Header() {
 
     return (
       <div className='w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4'>
-        <header className='flex w-ful max-w-7xl items-center justify-between px-4 mx-auto'>
+        <header className='flex w-full max-w-7xl items-center justify-between px-4 mx-auto'>
           <Link to="/">
           <img 
           src={logoImg}
--- Webcar/src/pages/home/index.tsx
+++ Webcar/src/pages/home/index.tsx
@@ -1,8 +1,15 @@
+import { Container } from "../../components/container";
+
 export function Home() {
     return (
-      <div>
-        <h1>Página Home</h1>
-      </div>
+      <Container>
+        <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
+          <input 
+          className="w-full border-2 rounded-lg h-9 px-3 outline-none"
+          placeholder="Digite o nome de um carro"/>
+          <button className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg">Buscar</button>
+        </section>
+      </Container>
     )
   
   }
